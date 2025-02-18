f8b54da11ffe30e6b74b8c690d2cf70343115bc2
Slidey <slideywotter@gmail.com>
2025-02-18 13:24:15 -0300

    Aceitar requisição de arquivos .js e .css

--- src/app_controler.py
+++ src/app_controler.py
@@ -2,24 +2,32 @@ from flask import request, jsonify, send_from_directory
 
 # --- Controlador da Aplicação ---
 class AppController:
-    """
-    Controlador que centraliza a configuração das rotas do Flask.
-    Separa a lógica de configuração das rotas da lógica de negócios, promovendo modularização.
-    """
-    def setup_routes(app, event_manager):
-        @app.get('/')
-        def index():
-            return send_from_directory('static', 'index.html')
+  """
+  Controlador que centraliza a configuração das rotas do Flask.
+  Separa a lógica de configuração das rotas da lógica de negócios, promovendo modularização.
+  """
+  def setup_routes(app, event_manager):
+    @app.get('/<path:pathname>.css')
+    def get_style(pathname):
+      return send_from_directory('static', pathname + '.css')
 
-        @app.post('/send')
-        def send_message():
-            # Extrai o JSON da requisição
-            data = request.json
-            message = data.get('message', '').strip()
-            if not message:
-                # Se a mensagem estiver vazia, dispara um evento de erro
-                event_manager.notify("ErrorEvent", "Mensagem vazia enviada")
-                return jsonify({"status": "Erro", "error": "Mensagem vazia"}), 400
-            # Dispara o evento de mensagem com o conteúdo recebido
-            event_manager.notify("MessageEvent", message)
-            return jsonify({"status": "Mensagem processada", "message": message})
+    @app.get('/<path:pathname>.js')
+    def get_script(pathname):
+      return send_from_directory('static', pathname + '.js')
+
+    @app.get('/')
+    def get_index():
+      return send_from_directory('static', 'index.html')
+
+    @app.post('/send')
+    def send_message():
+      # Extrai o JSON da requisição
+      data = request.json
+      message = data.get('message', '').strip()
+      if not message:
+        # Se a mensagem estiver vazia, dispara um evento de erro
+        event_manager.notify("ErrorEvent", "Mensagem vazia enviada")
+        return jsonify({"status": "Erro", "error": "Mensagem vazia"}), 400
+      # Dispara o evento de mensagem com o conteúdo recebido
+      event_manager.notify("MessageEvent", message)
+      return jsonify({"status": "Mensagem processada", "message": message})
